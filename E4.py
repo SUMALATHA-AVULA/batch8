c9c0f1bdf6b8e50dcd981adb7be0efe0be956574
SUMALATHA-AVULA <sumalathaavula9573@gmail.com>
2022-06-21 12:13:06 +0530

    adding in repo
    adding in repo

---
+++ E4.py
@@ -0,0 +1,17 @@
+"""Exercise 4: Remove first n characters from a string
+Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+For example:
+
+
+remove_chars("pynative", 4) so output must be tive. Here we need to remove first four characters from a string.
+remove_chars("pynative", 2) so output must be native. Here we need to remove first two characters from a string.
+Note: n must be less than the length of the string."""
+
+
+str=input("sample string: ")
+i=int(input("enter how many letters need to remove from front :"))
+print("Original String is ", str)
+print("Printing only even index chars")
+print(str[i:])
+
